9bf9733516a7a55e4168f1de92b8efa1d73d5b52
CamiN <roanstarsha@gmail.com>
2025-08-01 23:23:06 +0300

    Create project.js

---
+++ project.js
@@ -0,0 +1,13 @@
+export default function () {
+  return `
+    <section class="bg-green-50 py-16 px-6 max-w-5xl mx-auto">
+      <h3 class="text-3xl font-semibold text-center mb-8">Our Projects</h3>
+      <ul class="grid grid-cols-1 md:grid-cols-2 gap-6">
+        <li class="bg-white p-4 shadow rounded">Digital Literacy for Women & Youth</li>
+        <li class="bg-white p-4 shadow rounded">Community Internet Access Points</li>
+        <li class="bg-white p-4 shadow rounded">Digital Storytelling for Change</li>
+        <li class="bg-white p-4 shadow rounded">Blockchain Education & Web3 Bootcamps</li>
+      </ul>
+    </section>
+  `;
+}
